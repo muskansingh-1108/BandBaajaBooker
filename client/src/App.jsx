9d15398c29b363e5aa5554cddfcf32b5ae756488
muskansingh-1108 <200177237+muskansingh-1108@users.noreply.github.com>
2026-04-09 20:00:58 +0000

    initial

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import EventDetail from './pages/EventDetail';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import UserDashboard from './pages/UserDashboard.jsx';
-import AdminDashboard from './pages/AdminDashboard.jsx';
-import PaymentSuccess from './pages/PaymentSuccess.jsx';
-import PaymentFailed from './pages/PaymentFailed.jsx';
+import UserDashboard from './pages/UserDashboard';
+import AdminDashboard from './pages/AdminDashboard';
+import PaymentSuccess from './pages/PaymentSuccess';
+import PaymentFailed from './pages/PaymentFailed';
 
 function App() {
     return (
         <Router>
             <div className="min-h-screen bg-gray-50 flex flex-col">
-                <Navbar />
+                
                 <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8">
                     <Routes>
                         <Route path="/" element={<Home />} />
