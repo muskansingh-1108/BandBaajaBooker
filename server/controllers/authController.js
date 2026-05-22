d5489d9435b1e98ac3c11f6f2812890ec635f26e
muskansingh-1108 <200177237+muskansingh-1108@users.noreply.github.com>
2026-05-22 19:06:00 +0000

    ..

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -23,7 +23,7 @@ exports.register = async (req, res) => {
             name,
             email,
             password: hashedPassword,
-            role, // Hardcoded to prevent frontend passing role
+            role: 'user', // Hardcoded to prevent frontend passing role
             isVerified: false
         });
 
